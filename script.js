7164cb1830a1670c4adb160dc8c72103cc23a4ea
jithin-rajesh <jithin.ayalloor@gmail.com>
2024-12-29 19:45:17 +0530

    Made a console Tic Tac Toe gaming, play using game.playRound(column, row)

--- script.js
+++ script.js
@@ -6,20 +6,34 @@ function Gameboard() {
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(1);
+            board[i].push(0);
         }
     }
-    console.log(board);
 
     const getBoard = () => board;
     
+    
+
+    const placePiece = (column, row, player) => {      
+        board[column][row] = player;
+        console.log(`${player} placed at [${column}][${row}]`);        
+   }
+
+   const printBoard = () => {
+    console.log(board);
+    };
+
 
     return {
-        getBoard
+        getBoard,
+        placePiece,
+        printBoard
     };
 }
 
 Gameboard();
+
+
 
 function GameController(
     playerOneName = "Player One",
@@ -39,21 +53,82 @@ function GameController(
 
     let activePlayer = players[0];
 
-    const switchPlayer = () => {
+    const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
 
     const getActivePlayer = () => activePlayer;
 
     const printNewRound = () => {
-        board.prinBoard();
+        board.printBoard();
         console.log(`${getActivePlayer().name}'s turn.`);
     };
 
-    const playRound = (column) => {
-        console.log(`
-            Dropping ${getActivePlayer().name}'s token into column ${column}...`
-        );
-        board.drop
-    }
+    
+    const playRound = (column, row) => {
+        const boardArray = board.getBoard();
+        let newBoard;
+        if (column < 3 && row < 3 && column > -1  && row > -1 && boardArray[column][row] === 0)
+        {
+            console.log(`
+                ${getActivePlayer().name} has placed ${getActivePlayer().token} at [${column}][${row}]...`
+            );
+            board.placePiece(column, row, getActivePlayer().token);
+
+        }
+        else
+        {
+            console.log("Invalid position, Try Again");
+            printNewRound();
+            return;
+        }
+
+        if (findWinner()) {
+            board.printBoard();
+            console.log("Game Over");
+            return;
+        }
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    const findWinner = () => {
+        let currentToken = getActivePlayer().token;
+        let newBoard = board.getBoard();
+                
+        if (newBoard.some(row => row.every(cell => cell === currentToken)))
+        {
+            console.log(`${activePlayer.name} WINS!!`);
+            return true;
+        }
+
+        for (let i = 0; i < 3; i++)
+        {
+            if (newBoard.every(row => row[i] === currentToken)){
+                console.log(`${activePlayer.name} WINS!!`);
+                return true;
+            }
+        }
+        const leftDiagonal = [0, 1, 2].every(index => newBoard[index][index] === currentToken);
+        const rightDiagonal = [0, 1, 2].every(index => newBoard[index][2 - index] === currentToken);
+
+        if (leftDiagonal || rightDiagonal) {
+            console.log(`${activePlayer.name} WINS!!`);
+            return true;
+        }
+
+        
+        return false;
+        
+    };
+
+    printNewRound();
+
+    return{
+        playRound,
+        getActivePlayer,
+        findWinner
+    };
 }
+
+const game = GameController();
