7a2a123b70f4024ae4359df37f2d48f1a2cf9416
jithin-rajesh <jithin.ayalloor@gmail.com>
2024-12-31 23:42:45 +0530

    Added styles and functions including a line over winning elements

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function Gameboard() {
     const columns = 3;
     const board = [];
     const cellContainer = document.querySelector('.game');
+    const cellListeners = [];
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
@@ -35,55 +36,104 @@ function Gameboard() {
                 let img = document.createElement('img');
                 const cell = document.createElement('div');
                 cell.classList.add('box');
-                cell.addEventListener('click', () => {
-                    
-
+                if (i == 0)
+                {
+                    cell.style.borderTop = "none";
+                }
+                if (j == 2)
+                {
+                    cell.style.borderRight = "none";
+                }
+                if ((i == 2))
+                {
+                    cell.style.borderBottom = "none";
+                }
+                if (j == 0)
+                {
+                    cell.style.borderLeft = "none";
+                }
+                function handleClick() {
                     if(board[i][j] != 0)
-                    {
-                        console.log("Already Occupried, Try Again.");
-                        return;
-                    }
-                    game.playRound(i, j);
-
-                    const img = document.createElement('img');
-                    if(board[i][j] === 'X')
-                    {
-                        img.src = 'icons/x-symbol-svgrepo-com.svg'
-                    }
-                    else
-                    {
-                        img.src = 'icons/circle-svgrepo-com.svg'
-                    }
-                    img.style.maxWidth = '90%';
-                    img.style.maxHeight = '90%';
-                    cell.appendChild(img);
-                    if(game.findWinner()) {
-                        removeAllListeners();
-                    }
-                    renderCells;                    
-                 })
+                        {
+                            console.log("Already Occupried, Try Again.");
+                            return;
+                        }
+                        game.playRound(i, j);
+    
+                        const img = document.createElement('img');
+                        if(board[i][j] === 'X')
+                        {
+                            img.src = 'icons/x-symbol-svgrepo-com.svg'
+                        }
+                        else
+                        {
+                            img.src = 'icons/circle-svgrepo-com.svg'
+                        }
+                        img.style.maxWidth = '80%';
+                        img.style.maxHeight = '80%';
+                        cell.appendChild(img);
+                }
+                cell.addEventListener('click', (handleClick));
+                cellListeners.push({ cell, handler: handleClick});                  
                 cellContainer.appendChild(cell);
             }
         }       
 
     }
-
-    const removeAllListeners = () => {
-        const cells = document.querySelectorAll('.box');
-        cells.forEach(cell => {
-            const handleClick = cell.dataset.listener;
-            if (handleClick) {
-                cell.removeEventListener('click', handleClick);
-            }
-        });
-    };
     
+    const removeAllListeners = () => {
+        cellListeners.forEach(({ cell, handler}) => {
+            cell.removeEventListener("click", handler);
+        });
+        cellListeners.length = 0
+    };
+
+    const drawWinningLine = (winType, index) => {
+        const line = document.createElement('div');
+        line.classList.add('winning-line');
+        
+        switch(winType) {
+            case 'row':
+                line.style.cssText = `
+                    position: absolute;
+                    height: 0.8em;
+                    background: white;
+                    width: 100%;
+                    top: ${(index * 33.33) + 15}%;
+                `;
+                break;
+            case 'column':
+                line.style.cssText = `
+                    position: absolute;
+                    width: 0.8em;
+                    background: white;
+                    height: 100%;
+                    left: ${(index * 33.33) + 15}%;
+                `;
+                break;
+            case 'diagonal':
+                line.style.cssText = `
+                    position: absolute;
+                    height: 10px;
+                    background: white;
+                    width: 140%;
+                    top: 48%;
+                    left: -20%;
+                    transform: ${index === 0 ? 'rotate(45deg)' : 'rotate(-45deg)'};
+                `;
+                break;
+        }
+
+        cellContainer.appendChild(line);
+    };
 
     return {
         getBoard,
         placePiece,
         printBoard,
-        renderCells
+        renderCells,
+        removeAllListeners,
+        drawWinningLine
     };
 }
 
@@ -154,32 +204,46 @@ function GameController(
     const findWinner = () => {
         let currentToken = getActivePlayer().token;
         let newBoard = board.getBoard();
-                
-        if (newBoard.some(row => row.every(cell => cell === currentToken)))
-        {
-            console.log(`${activePlayer.name} WINS!!`);
-            return true;
-        }
-
-        for (let i = 0; i < 3; i++)
-        {
-            if (newBoard.every(row => row[i] === currentToken)){
+        
+        for (let i = 0; i < 3; i++) {
+            if (newBoard[i].every(cell => cell === currentToken)) {
+                board.drawWinningLine('row', i);
+                board.removeAllListeners();
                 console.log(`${activePlayer.name} WINS!!`);
                 return true;
             }
         }
-        const leftDiagonal = [0, 1, 2].every(index => newBoard[index][index] === currentToken);
-        const rightDiagonal = [0, 1, 2].every(index => newBoard[index][2 - index] === currentToken);
 
-        if (leftDiagonal || rightDiagonal) {
+        for (let i = 0; i < 3; i++) {
+            if (newBoard.every(row => row[i] === currentToken)) {
+                board.drawWinningLine('column', i);
+                board.removeAllListeners();
+                console.log(`${activePlayer.name} WINS!!`);
+                return true;
+            }
+        }
+        
+        if ([0, 1, 2].every(i => newBoard[i][i] === currentToken)) {
+            board.drawWinningLine('diagonal', 0);
+            board.removeAllListeners();
             console.log(`${activePlayer.name} WINS!!`);
             return true;
         }
 
-        
+        if ([0, 1, 2].every(i => newBoard[i][2-i] === currentToken)) {
+            board.drawWinningLine('diagonal', 1);
+            board.removeAllListeners();
+            console.log(`${activePlayer.name} WINS!!`);
+            return true;
+        }
+
+        if (newBoard.every(row => row.every(cell => cell !== 0))) {
+            board.removeAllListeners();
+            console.log("Game is a draw!");
+            return true;
+        }
         return false;
-        
-    };
+    }
 
     
 
